f34b30b027725a05f332e3625aaa919ee4b3699f
shohammallick25 <shohammallick528@gmail.com>
2025-07-04 23:14:27 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,49 @@
 const CACHE_NAME = "teachzone-cache-v1";
 const urlsToCache = [
-"/",
-"/index.html",
-"/css/main.css",
-"/js/script.js",
-"/images/logo.png",
-// add all other important files you want offline
+  "/",
+  "/index.html",
+  "/css/normalize.css",
+  "/css/utilities.css",
+  "/css/main.css",
+  "/plugins/slick-1.8.1/slick/slick.css",
+  "/plugins/slick-1.8.1/slick/slick-theme.css",
+  "/js/script.js",
+  "/images/logo.png"
 ];
 
+// INSTALL: cache essential files
 self.addEventListener("install", event => {
-event.waitUntil(
-caches.open(CACHE_NAME).then(cache => {
-return cache.addAll(urlsToCache);
-})
-);
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting(); // activate immediately
 });
 
+// ACTIVATE: clean up old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH: serve cache or network
 self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request).then(response => {
-return response || fetch(event.request);
-})
-);
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        if (event.request.destination === "document") {
+          return caches.match("/index.html");
+        }
+      });
+    })
+  );
 });
