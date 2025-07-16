b176a0bab1018a1bcac6f6c3436e91b6f53e59e9
shohammallick25 <shohammallick528@gmail.com>
2025-07-16 19:17:06 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,21 +2,37 @@ const CACHE_NAME = "teachzone-cache-v1";
 const urlsToCache = [
   "/",
   "/index.html",
-  "/css/normalize.css",
   "/css/main.css",
-  "/js/script.js",
+  "/css/utilities.css",
   "/images/logo.png",
-  // add other assets
+  // add other assets like images and JS files here
 ];
 
-self.addEventListener("install", event => {
+// Install service worker
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+// Activate service worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch cache
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
